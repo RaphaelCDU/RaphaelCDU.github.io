12bc6e7b74c7f5befbb6dfe9cf1e11f6dd5762a6
RaphaelCDU <110903625+RaphaelCDU@users.noreply.github.com>
2022-08-14 00:52:45 +0930

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -3,7 +3,7 @@ var mark = 0;
 
 for(pos = 0; pos < quizF.q1RB.length; pos++){ 
 
-//the 'for' statment is used to create a loop. the loop is useful becasue it allows someone to redo the test without refreshing the page. this is acheived by always resetting the pos variable to 0 at the start of every iteration. because pos is less than 4 (quizF.q1RB.length), the command executes
+// the 'for' statment is used to create a loop. — The loop is used to check each corresponding radio button option, in question 1, per iteration. — This is achieved by initially setting the pos variable to 0; then, because the pos variable value is less than 4 (quizF.q1RB.length), the below code it executed. — Each time the loop completes an iteration, the value of pos increases by one, thus moving onto the next Radio-button which corresponds to that value. — This continues until pos  is no longer less than 4, also signalling that all the radio-buttons options have been checked, and in which case the loop ceases; and moves onto the  next code in the sequence. 
 
 currentButton = document.quizF.q1RB [pos];
 if(currentButton.checked){
